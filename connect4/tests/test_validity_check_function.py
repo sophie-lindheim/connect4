23017eed899bb26c8b3956e3b517f35de27376d1
kgyory <krisztian.gyoery@edu.campus02.at>
2021-01-27 15:31:54 +0100

    completed unit test for validity check function

--- connect4/tests/test_validity_check_function.py
+++ connect4/tests/test_validity_check_function.py
@@ -5,6 +5,14 @@ from connect4.c4_class import C4Game
 
 
 class ValidityTestCase(unittest.TestCase):
+    """Unittest to test the validity_check function.
+
+       In this test the validity check function of th econnect4 game is tested. 3 cases are tested, the first
+       method tests if the function reacts correctly if an input comes to an already full column.
+       The second method tests, if the function reacts correctly if the input is not a valid column letter.
+       The third tests is the function changes the right variable to the right value with a valid input.
+
+       """
 
     def setUp(self):
         self.actual_player = 1
@@ -22,14 +30,14 @@ class ValidityTestCase(unittest.TestCase):
         C4Game.validity_check(self)
         mock_print.assert_called_with("Column already full, pick another")
 
-    # checking output if player entered an invalid column letter
+    # checking output if player entered an invalid column letter:
     @patch('builtins.print')
     def test_wrong_column(self, mock_print):
         self.c4_input = 'p'
         C4Game.validity_check(self)
         mock_print.assert_called_with("No such column, input must be a, b, c, d, e, f or g; or x to end game")
 
-    # checking if variable will have the right value if the input is valid
+    # checking if variable will have the right value if the input is valid:
     def test_valid_input(self):
         self.c4_input = 'c'
         C4Game.validity_check(self)
